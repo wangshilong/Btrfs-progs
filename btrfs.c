9e5bd6d2d231b2ddc3588ffccba9968677318559
Wang Shilong <wangsl.fnst@cn.fujitsu.com>
2013-09-09 23:50:01 +0800

    Btrfs-progs: add btrfs-recover-superblock to recover bad supers
    Until now if one of device's first superblock is corrupt,
    btrfs will fail to mount. Luckily, btrfs have at least two
    superblocks for every disk.
    
    In theory, if silent corrupting happens when we are writting
    superblocks into disk. we must hold at leat one good superblock.
    
    This little program will try to correct bad superblocks from
    good superblocks with max generation.
    
    Signed-off-by: Wang Shilong <wangsl.fnst@cn.fujitsu.com>

--- btrfs.c
+++ btrfs.c
@@ -248,6 +248,7 @@ static const struct cmd_group btrfs_cmd_group = {
 		{ "scrub", cmd_scrub, NULL, &scrub_cmd_group, 0 },
 		{ "check", cmd_check, cmd_check_usage, NULL, 0 },
 		{ "chunk-recover", cmd_chunk_recover, cmd_chunk_recover_usage, NULL, 0},
+		{ "super-recover", cmd_super_recover, cmd_super_recover_usage, NULL, 0},
 		{ "restore", cmd_restore, cmd_restore_usage, NULL, 0 },
 		{ "inspect-internal", cmd_inspect, NULL, &inspect_cmd_group, 0 },
 		{ "send", cmd_send, cmd_send_usage, NULL, 0 },
